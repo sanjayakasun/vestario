5ecea99bcf195971fd120236b0a5962283a9cf3a
Malsha Karunarathna <mkarunarathna872@gmail.com>
2023-10-24 13:39:04 +0530

    Update response.js

--- response.js
+++ response.js
@@ -3,5 +3,10 @@ const responseObj={
     fine:"Hey! What are you looking for:services , today , time , customize_product , reveiws , contact_us",
     today:new Date().toDateString(),
     time:new Date().toLocaleTimeString(),
-    services: "Vestario provides you clothes for comfortable prices"
+    services: "Vestario provides you clothes for comfortable prices",
+    customize_product:"copy and paste this Link:-> http://localhost/vestario/design.php ",
+    New_Arrivals:"copy and paste this Link:->",
+    Offers:"copy and paste this Link:->",
+    reveiws:"copy and paste this Link:-> http://localhost/vestario/review.php",
+    contact_us:"copy and paste this Link:-> http://localhost/vestario/contactus.php"
 };
