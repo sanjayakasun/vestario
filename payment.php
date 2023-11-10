12a9dfb74dd3b47e0ce8664117e8ff93446005bb
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2023-11-10 11:49:54 +0530

    new

--- payment.php
+++ payment.php
@@ -249,7 +249,7 @@ if (isset($_POST['pay'])) {
                 <ul class="list-group list-group-flush">
                   <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                     <div>
-                      <strong>Total amount : </strong>
+                      <strong>Total Amount : </strong>
                       <strong> </strong>
                     </div>
                     <?php 
